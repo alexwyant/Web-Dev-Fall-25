971dd481622f01a90dac63779b3bfb721e9add8d
alexwyant <wyanta1@newpaltz.edu>
2025-12-15 13:44:22 -0500

    Updated index.js

--- Complete Project /index.js
+++ Complete Project /index.js
@@ -1,22 +1,31 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
+const path = require("path");
 const app = express();
 
 app.use(express.json());
 
-const userRoutes = require('./server/routes/user');
-const expenseRoutes = require('./server/routes/expense'); 
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");  
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");  
+// Routes
+const userRoutes = require("./server/routes/user");
+const expenseRoutes = require("./server/routes/expense");
+
+// CORS headers (fine to keep)
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   next();
 });
 
-app.use('/users', userRoutes);
-app.use('/expenses', expenseRoutes);
+app.use("/users", userRoutes);
+app.use("/expenses", expenseRoutes);
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`Server started on port ${PORT}!!`))
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "register.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}!!`));
